83d40eaa375336cb10894455dd797449c393a0ac
Rodan1618 <rodan1618@gmail.com>
2026-02-13 10:28:10 -0300

    Update if_elif_else_SistRecomCtdo.py

--- Exercise.py/if_elif_else_SistRecomCtdo.py
+++ Exercise.py/if_elif_else_SistRecomCtdo.py
@@ -1,6 +1,9 @@
 #Caso de Uso:
 # Sistema de Recomendação de Conteúdo #################
-# Imagine um sistema de recomendação de conteúdo para uma plataforma de streaming. As condicionais podem ser utilizadas para personalizar as recomendações com base nas preferências de idade do usuário. Por exemplo, um usuário jovem pode receber recomendações de filmes e séries animadas, enquanto um usuário adulto pode receber sugestões de dramas e documentários.
+# Imagine um sistema de recomendação de conteúdo para uma plataforma de streaming.
+#As condicionais podem ser utilizadas para personalizar as recomendações com base nas
+#preferências de idade do usuário. Por exemplo, um usuário jovem pode receber recomendações
+#de filmes e séries animadas, enquanto um usuário adulto pode receber sugestões de dramas e documentários.
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 idade = int(input("Digite sua idade: "))
@@ -11,4 +14,5 @@ elif idade < 18:
 elif idade < 60:
     print("Recomendamos dramas, documentários e thrillers.")
 else:
+
     print("Recomendamos clássicos e filmes com temáticas históricas.")    
